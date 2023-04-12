32494071cc7df48ae19ab94aba277425fd762bcd
Regi-na <64543329+Regi-na@users.noreply.github.com>
2023-04-12 10:32:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from hyppo.ksample import Energy, MMD
 
 chat_id = 947352272 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +8,5 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return MMD(compute_kernel="laplacian", gamma=1).test(x, y)[1] < 0.06
+
